fbca6fcb9c8e6e9c924aa24759bd1b1a4e60ee82
AndreyDMA <andreydmitriev@ukr.net>
2019-08-21 16:34:13 +0300

    first try solution

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,21 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        if (n == 0) {
+            return new int[0];
+        }
+        if (n == 1) {
+            return new int[1];
+        } else {
+            int[] sequence = new int[n];
+            sequence[0] = 0;
+            sequence[1] = 1;
+            for (int i = 2; i < n; i++) {
+                sequence[i] = sequence[i - 2] + sequence[i - 1];
+            }
+            return sequence;
+        }
+
     }
+
 }
