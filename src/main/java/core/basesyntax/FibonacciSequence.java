4f1937fd53940e64eeb7bd9fdd4f672f80367b84
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-21 16:14:37 +0300

    added solution for fibonacci homework

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,21 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        int[] res = new int[n];
+        if (n == 0) {
+            return res;
+        } else if (n == 1) {
+            res[0] = 0;
+        } else if (n == 2) {
+            res[0] = 0;
+            res[1] = 1;
+        } else {
+            res[0] = 0;
+            res[1] = 1;
+            for (int i = 2; i < n; i++) {
+                res[i] = res[i - 1] + res[i - 2];
+            }
+        }
+        return res;
     }
 }
