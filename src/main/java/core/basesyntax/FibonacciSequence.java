3e59ee5cdc8e10bb6cb4ce5c05cebb1ca8b9631f
Kokhanevich <nike.k1995@gmail.com>
2019-08-21 15:55:02 +0300

    added solution for HW3-fibonacci

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,20 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        int [] mass = new int[n];
+        for (int i = 0; i < mass.length; i++) {
+            mass [i] = fibonacciRec(i);
+        }
+        return mass;
+    }
+
+    private int fibonacciRec(int s) {
+        if (s == 0) {
+            return 0;
+        } else if (s == 1) {
+            return 1;
+        } else {
+            return fibonacciRec(s - 1) + fibonacciRec(s - 2);
+        }
     }
 }
