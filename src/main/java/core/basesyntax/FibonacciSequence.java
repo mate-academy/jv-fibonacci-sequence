6b869699037918171b2bdb2d5622a12d3ceb7288
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-21 12:23:51 +0300

    Update FibonacciSequence.java

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,18 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        if (n == 0) {
+            return new int[]{};
+        }
+        if (n == 1) {
+            return new int[]{0};
+        }
+        int[] returnArray = new int[n];
+        returnArray[0] = 0;
+        returnArray[1] = 1;
+        for (int i = 2; i < n; i++) {
+            returnArray[i] = returnArray[i - 1] + returnArray[i - 2];
+        }
+        return returnArray;
     }
 }
