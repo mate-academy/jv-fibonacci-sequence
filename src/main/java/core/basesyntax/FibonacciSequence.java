303cba93f7079060f99d8e914ea7ee6b1148bd16
lenovo <suleimanovayev@gmail.com>
2019-08-21 20:20:42 +0300

    add solution fibonachi

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,18 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        int num1 = 0;
+        int num2 = 0;
+        int[] fibonachiNum = new int[n];
+        for (int i = 0; i < n ; i++) {
+            if (i == 1) {
+                num1 = 1;
+            }
+            fibonachiNum[i] = num1 + num2;
+            num1 = num2;
+            num2 = fibonachiNum[i];
+        }
+        return fibonachiNum;
     }
+
 }
