40adf497c361b61d47bdb02e152bdc4ec8aa0af5
AdminPC\Admin <admin2k7@gmail.com>
2019-08-21 16:33:58 +0300

    3.3 task added

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,18 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        if (n == 0) {
+            return new int[]{};
+        } else if (n == 1) {
+            return new int[]{1};
+        } else {
+            int[] fibonacciArray = new int[n];
+            fibonacciArray[1] = 1;
+            for (int i = 2; i < n; i++) {
+                fibonacciArray[i] =
+                        fibonacciArray[i - 2] + fibonacciArray[i - 1];
+            }
+            return fibonacciArray;
+        }
     }
 }
