a3dedff5db144823811ea32681d8b1bf57a72db2
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-21 16:51:39 +0300

    fibonacci method

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -5,7 +5,22 @@ public class FibonacciSequence {
      * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
      * из `n`чисел  последовательности Фибоначчи.
      */
-    public int[] getFibonacciSequence(int n) {
-        return null;
+    public static int[] getFibonacciSequence(int n) {
+        int[] f = new int[n];
+
+        for (int i = 0; i < f.length; ++i) {
+            if (i == 0) {
+                f[i] = 0;
+            } else if (i == 1) {
+                f[i] = 1;
+            } else {
+
+                f[i] = f[i - 1] + f[i - 2];
+            }
+        }
+        return f;
     }
 }
+
+
+
