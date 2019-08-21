b8cf0c1fd57a71be4ae4ab19f31e6520f6a2a9a4
Iurii <kenu21@gmail.com>
2019-08-21 22:20:10 +0300

    added hw-3-3

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,22 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        if (n == 0) {
+            return new int[0];
+        }
+        if (n == 1) {
+            return new int[] { 0 };
+        }
+        if (n == 2) {
+            return new int[] { 0, 1 };
+        }
+
+        int[] ar = new int[n];
+        ar[0] = 0;
+        ar[1] = 1;
+        for (int i = 2; i < n; i++) {
+            ar[i] = ar[i - 2] + ar[i - 1];
+        }
+        return ar;
     }
 }
