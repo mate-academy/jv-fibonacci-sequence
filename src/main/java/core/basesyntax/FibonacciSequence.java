0220cee91adcab5ec5d6ac669e06cb057cd80bae
Max <skaterweather41@gmail.com>
2019-08-21 18:29:33 +0200

    added solution to task 3 fibonacciSequence

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,17 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        if (n == 1) {
+            return new int[] {0};
+        }
+        int[] fibonacciSequence = new int[n];
+        if (n > 1) {
+            fibonacciSequence[0] = 0;
+            fibonacciSequence[1] = 1;
+        }
+        for (int i = 2; i < n; i++) {
+            fibonacciSequence[i] = fibonacciSequence[i - 2 ] + fibonacciSequence[i - 1];
+        }
+        return fibonacciSequence;
     }
 }
