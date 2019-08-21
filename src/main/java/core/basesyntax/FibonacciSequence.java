82aa303827853f613fb50f13b2e919bd602aab5f
Тарас <tarashryts@gmail.com>
2019-08-21 17:12:53 +0300

    hw-33-fibonachi

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -5,22 +5,23 @@ public class FibonacciSequence {
      * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
      * из `n`чисел  последовательности Фибоначчи.
      */
-    public int[] getFibonacciSequence(int n) {
-        int [] arrayOfNumbersFibonacci = new int[n];
-        if (n >= 1) {
+    public static int[] getFibonacciSequence(int n) {
+        int[] arrayOfNumbersFibonacci = new int[n];
+        if (n == 1) {
             arrayOfNumbersFibonacci[0] = 0;
         }
-        if (n >= 2) {
+        if (n == 2) {
+            arrayOfNumbersFibonacci[0] = 0;
             arrayOfNumbersFibonacci[1] = 1;
         }
-
-        if (n > 2) {
-            for (int i = 2; i < n; i++) {
-                arrayOfNumbersFibonacci[i] = arrayOfNumbersFibonacci[i - 1]
-                        + arrayOfNumbersFibonacci[i - 2];
+        if (n >= 3) {
+            int[] tempArray = getFibonacciSequence(n - 1);
+            for (int i = 0; i < n - 1; i++) {
+                arrayOfNumbersFibonacci[i] = tempArray[i];
             }
+            arrayOfNumbersFibonacci[n - 1] = arrayOfNumbersFibonacci[n - 3]
+                    + arrayOfNumbersFibonacci[n - 2];
         }
-
         return arrayOfNumbersFibonacci;
     }
 }
