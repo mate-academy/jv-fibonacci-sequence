7f073b5460d1ffd355ad3f4ae764e88a60b92aaf
Rosinskyi <sergey22061993@gmail.com>
2019-08-21 15:03:29 +0300

    added solution for jv-fibonacci-sequence

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -5,7 +5,15 @@ public class FibonacciSequence {
      * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
      * из `n`чисел  последовательности Фибоначчи.
      */
-    public int[] getFibonacciSequence(int n) {
-        return null;
+    public static int[] getFibonacciSequence(int n) {
+        int[] fibonacci = new int[n];
+        int value = 0;
+        int value1 = 1;
+        for (int i = 0; i < n; i++) {
+            fibonacci[i] = value;
+            value = value1;
+            value1 += fibonacci[i];
+        }
+        return fibonacci;
     }
 }
