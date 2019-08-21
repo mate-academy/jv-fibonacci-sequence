5e7670adcbd7b48b7bda7015f0e816bd8b01e2b8
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-08-21 16:14:51 +0300

    added solution with fibonacci sequence

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -1,11 +1,22 @@
 package core.basesyntax;
 
 public class FibonacciSequence {
+
     /**
-     * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
-     * из `n`чисел  последовательности Фибоначчи.
+     * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий из
+     * `n`чисел  последовательности Фибоначчи.
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        int[] fibonacciSequence = new int[n];
+        for (int i = 0; i < fibonacciSequence.length; i++) {
+            if (i == 0) {
+                fibonacciSequence[i] = 0;
+            } else if (i == 1) {
+                fibonacciSequence[i] = 1;
+            } else {
+                fibonacciSequence[i] = fibonacciSequence[i - 2] + fibonacciSequence[i - 1];
+            }
+        }
+        return fibonacciSequence;
     }
 }
