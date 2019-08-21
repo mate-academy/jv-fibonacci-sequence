a83d485ab37d705f7752995af4f70bc6db9f00a8
NikolayKatsai <alaska.rok123@gmail.com>
2019-08-21 17:00:15 +0300

    getFibonacciSequence method finished in FibonacciSequence class

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,6 +6,21 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.</p>
      */
     public int[] getFibonacciSequence(int n) {
-        return null;
+        if (n <= 0) {
+            return new int[]{};
+        }
+        if (n == 1) {
+            return new int[]{0};
+        }
+        if (n == 2) {
+            return new int[]{0, 1};
+        }
+        int[] sequence = new int[n];
+        int[] prvSequence = getFibonacciSequence(n - 1);
+        for (int i = 0; i < prvSequence.length; i++) {
+            sequence[i] = prvSequence[i];
+        }
+        sequence[n - 1] = sequence[n - 2] + sequence[n - 3];
+        return sequence;
     }
 }
