d35bf8e6cfe368a21287228bebc13243fac6bdef
kuhaj <alex.kukhtin.dev@gmail.com>
2019-08-22 08:37:06 +0300

    without recursion

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -1,27 +1,23 @@
 package core.basesyntax;
 
-import java.util.stream.Stream;
-
 public class FibonacciSequence {
     /**
      * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
      * из `n`чисел  последовательности Фибоначчи.
-     *
-     * @return
      */
     public int [] getFibonacciSequence(int n) {
         if (n == 0) {
-            return new int[]{};
-        }
-        if (n == 1) {
+            return new int[0];
+        } else if (n == 1) {
             return new int[]{0};
+        } else {
+            int[] fibonacciArray = new int[n];
+            fibonacciArray[1] = 1;
+            for (int i = 2; i < n; i++) {
+                fibonacciArray[i] =
+                        fibonacciArray[i - 2] + fibonacciArray[i - 1];
+            }
+            return fibonacciArray;
         }
-        int[] array = new int[n];
-        array[0] = 0;
-        array[1] = 1;
-        for (int i = 2; i < n; i++) {
-            array[i] = array[i - 1] + array[i - 2];
-        }
-        return array;
     }
 }
