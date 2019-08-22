8b7fdad77fb5ce6dd0b18ea1a214f443c2870a14
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-22 17:40:58 +0300

    fibonacci variabel chanched

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -6,19 +6,19 @@ public class FibonacciSequence {
      * из `n`чисел  последовательности Фибоначчи.
      */
     public static int[] getFibonacciSequence(int n) {
-        int[] fibonacciNumbers = new int[n];
+        int[] fibonacciNumber = new int[n];
 
-        for (int k = 0; k < fibonacciNumbers.length; ++k) {
+        for (int k = 0; k < fibonacciNumber.length; ++k) {
             if (k == 0) {
-                fibonacciNumbers[k] = 0;
+                fibonacciNumber[k] = 0;
             } else if (k == 1) {
-                fibonacciNumbers[k] = 1;
+                fibonacciNumber[k] = 1;
             } else {
 
-                fibonacciNumbers[k] = fibonacciNumbers[k - 1] + fibonacciNumbers[k - 2];
+                fibonacciNumber[k] = fibonacciNumber[k - 1] + fibonacciNumber[k - 2];
             }
         }
-        return fibonacciNumbers;
+        return fibonacciNumber;
     }
 }
 
