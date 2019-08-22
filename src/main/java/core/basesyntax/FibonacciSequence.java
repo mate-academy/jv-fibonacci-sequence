a86c3c91d27403af4769a65c5c6f6bd1f46866fa
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-22 17:19:33 +0300

    Update FibonacciSequence.java

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -1,11 +1,24 @@
 package core.basesyntax;
 
 public class FibonacciSequence {
-    /**
-     * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
-     * из `n`чисел  последовательности Фибоначчи.
-     */
+
     public int[] getFibonacciSequence(int n) {
-        return null;
+        int[] numbers;
+        if (n == 0) {
+            numbers = new int[0];
+            return numbers;
+        } else if (n == 1) {
+            numbers = new int[1];
+            numbers[0] = 0;
+            return numbers;
+        }
+        numbers = new int[n];
+        numbers[0] = 0;
+        numbers[1] = 1;
+        for (int i = 2; i < n; i++) {
+            numbers[i] = numbers[i - 1] + numbers[i - 2];
+        }
+        return numbers;
     }
+
 }
