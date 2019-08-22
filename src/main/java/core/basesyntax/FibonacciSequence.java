df0cec0192c7186abb9997f89fd78eee0b9f540f
Koval <xui>
2019-08-23 00:52:09 +0300

    Fib

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -15,8 +15,8 @@ public class FibonacciSequence {
         int[] fib = new int[n];
         fib[0] = 0;
         fib[1] = 1;
-
-        for (int i = 2; i < n; i++) {
+        fib[2] = 1;
+        for (int i = 3; i < n; i++) {
             fib[i] = fib[i - 2] + fib[i - 1];
         }
         return fib;
