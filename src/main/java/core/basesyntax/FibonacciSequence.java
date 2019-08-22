009901839265541d14fbffa08458472d57768232
Andrew <tolstuy1996@gmail.com>
2019-08-22 07:31:34 +0300

    Fibonacci without recursion

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -5,7 +5,23 @@ public class FibonacciSequence {
      * Реализуйте метод `getFibonacciSequence(int n)` который вовзращает массив состоящий
      * из `n`чисел  последовательности Фибоначчи.
      */
+    public static void main(String[] args) {
+
+    }
+
     public int[] getFibonacciSequence(int n) {
-        return null;
+        if (n == 0) {
+            return new int[0];
+        }
+        if (n == 1) {
+            return new int[]{0};
+        }
+        int[] arrFibonacciNum = new int[n];
+        arrFibonacciNum[0] = 0;
+        arrFibonacciNum[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arrFibonacciNum[i] = arrFibonacciNum[i - 2] + arrFibonacciNum[i - 1];
+        }
+        return arrFibonacciNum;
     }
 }
