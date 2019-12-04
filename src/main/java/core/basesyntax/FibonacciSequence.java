2d7baaf565ceadae4071abf6a6b5adb5f3f9783e
levko <levkosvd@gmail.com>
2019-12-04 12:49:07 +0200

    hw3.3 - fibonacci v1

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -10,6 +10,12 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int index) {
-        return 0;
+
+        if (index == 1 || index == 2) {
+            return 1;
+        } else if (index == 0) {
+            return 0;
+        }
+        return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 }
