4eb8ca5cce6705dedb30404133c39f3b27e9b044
Julianevinska <57527101+Julianevinska@users.noreply.github.com>
2019-12-04 16:23:12 +0200

    HW 3.3 - solution

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -10,6 +10,7 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int index) {
-        return 0;
+        return index <= 1 ? (Math.max(index, 0))
+                : (getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2));
     }
 }
