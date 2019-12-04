d62eb05e810347ce519541394fe5c3f782797745
SergiyAgeev <sergiyageev@gmail.com>
2019-12-04 20:38:25 +0200

    hw 3 3 solution update

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -13,7 +13,7 @@ public class FibonacciSequence {
         if (index == 0) {
             return 0;
         }
-        if (index == 1 || index == 2) {
+        if (index == 1) {
             return 1;
         }
         return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
