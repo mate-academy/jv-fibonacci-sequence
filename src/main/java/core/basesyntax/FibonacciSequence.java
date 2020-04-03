fc8893b5fe587bd73b7e2eb246594578c3bd6834
petrukillya <petrukillya98@gmail.com>
2020-04-03 17:18:01 +0300

    fixed code style issue

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -11,6 +11,6 @@ public class FibonacciSequence {
      */
     public int getFibonacciNumber(int index) {
         return index == 0 ? 0 : index == 1 ? 1 :
-                getFibonacciNumber(index -1) + getFibonacciNumber( index -2);
+                getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 }
