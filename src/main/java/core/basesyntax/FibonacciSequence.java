fb290ea9b14f0fb98cc6f69b8b666dae10fcfbae
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-04-05 18:02:15 +0300

    added the solution

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -9,7 +9,14 @@ public class FibonacciSequence {
      * <p>Пример: n = 8
      * Результат: 21</p>
      */
+
     public int getFibonacciNumber(int index) {
-        return 0;
+        if (index == 0) {
+            return 0;
+        } else if (index == 1) {
+            return 1;
+        } else {
+            return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
+        }
     }
 }
