470708c0c0f7a124d2db706f31b97f5fbf2418b6
xeddin <61269748+xeddin@users.noreply.github.com>
2020-04-06 10:14:55 +0300

    Removed ELSE

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -14,8 +14,7 @@ public class FibonacciSequence {
             return 0;
         } else if (index == 1) {
             return 1;
-        } else {
-            return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
         }
+        return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 }
