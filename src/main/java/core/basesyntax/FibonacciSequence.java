6fb9b7cc90436f4939342bccf9012c8516b2eab4
Maksym Vakuliuk <vakuliukmaksym@mail.com>
2020-04-06 11:46:30 +0300

    smplified getFibonacciNumber(int index) to one line

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -10,7 +10,6 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int index) {
-        if (index <= 1) return index;
-        return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
+        return (index <= 1) ? index : getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 }
