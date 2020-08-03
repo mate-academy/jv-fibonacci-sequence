ca3e091c6729ef9a66c59706ea8fda689226407f
Roman <matviichuk.office@gmail.com>
2020-08-03 14:33:20 +0300

    added solution

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -1,5 +1,8 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibonacciSequence {
     /**
      * <p>Реализуйте метод `getFibonacciNumber(int n)` который возвращает число Фибоначи которое
@@ -10,6 +13,24 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int index) {
-        return 0;
+        if (index == 0) {
+            return 0;
+        }
+        if (index == 1) {
+            return 1;
+        }
+        int n1 = 0;
+        int n2 = 1;
+        int buffer = 0;
+        List<Integer> fibonacci = new ArrayList<>();
+        fibonacci.add(n1);
+        fibonacci.add(n2);
+        while (buffer <= 4181) {
+            buffer = n1 + n2;
+            n1 = n2;
+            n2 = buffer;
+            fibonacci.add(buffer);
+        }
+        return fibonacci.get(index);
     }
 }
