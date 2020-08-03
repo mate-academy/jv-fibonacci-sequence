e6e3e41560c296ac95285fe9a9545f5f2551e4ff
Dubovaya <dubovaya.java@gmail.com>
2020-08-03 21:34:59 +0300

    solution lera-6.4

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -1,15 +1,26 @@
 package core.basesyntax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibonacciSequence {
-    /**
-     * <p>Реализуйте метод `getFibonacciNumber(int n)` который возвращает число Фибоначи которое
-     * стоит на `n` позиции в последовательности Фибоначчи (начало нумерации с нуля).
-     * Используйте рекурсию.</p>
-     *
-     * <p>Пример: n = 8
-     * Результат: 21</p>
-     */
+
     public int getFibonacciNumber(int index) {
-        return 0;
+        List<Integer> fibonacciNumbers = new ArrayList();
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i <= index; i++) {
+            if (i == 2) {
+                j += (i - 2);
+            } else if (i > 2) {
+                j = fibonacciNumbers.get(i - 1) + fibonacciNumbers.get(k - 2);
+            } else {
+                j += i;
+            }
+            fibonacciNumbers.add(j);
+            k++;
+        }
+        return fibonacciNumbers.get(fibonacciNumbers.size() - 1);
     }
 }
+
