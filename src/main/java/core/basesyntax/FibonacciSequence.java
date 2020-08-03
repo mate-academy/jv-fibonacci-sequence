f28980f480f0dfe1edc6768732c8fc203dd8a3d3
romanHreits <romagreic@gmail.com>
2020-08-03 16:46:18 +0300

    ready solution hw-6.3

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -10,6 +10,15 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int index) {
-        return 0;
+        int result;
+
+        if (index == 0) {
+            return 0;
+        }
+        if (index == 1) {
+            return 1;
+        }
+        result = getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
+        return result;
     }
 }
