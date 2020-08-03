d9125a99239748021287d1920294235a9e972224
Vladislav <48126402+vladislavshvets@users.noreply.github.com>
2020-08-03 22:49:08 +0300

    added solution

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -10,6 +10,17 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int index) {
-        return 0;
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+        for (int i = 3; i <= index; i++) {
+            n2 = n0 + n1;
+            n0 = n1;
+            n1 = n2;
+            if (i == index) {
+                return n2;
+            }
+        }
+        return index > 0 ? 1 : 0;
     }
 }
