cf22712cdac94eaa6539cfb32cdec7f9ee2d237a
vosker <nikita.vaganov16@gmail.com>
2020-08-04 09:24:03 +0300

    fixed mistake

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -10,9 +10,8 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int index) {
-        int result = 1;
-        return (index == 0) ? result - 1 :
-                (index == 1 || index == 2) ? result :
+        return (index == 0) ? 0 :
+                (index == 1 || index == 2) ? 1 :
                         getFibonacciNumber(index - 2) + getFibonacciNumber(index - 1);
     }
 }
