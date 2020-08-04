78ef6af354dd6775100186d94b6b1b549769031d
Dubovaya <dubovaya.java@gmail.com>
2020-08-04 12:05:51 +0300

    add Fibonacci func

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -1,26 +1,11 @@
 package core.basesyntax;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FibonacciSequence {
 
     public int getFibonacciNumber(int index) {
-        List<Integer> fibonacciNumbers = new ArrayList();
-        int j = 0;
-        int k = 0;
-        for (int i = 0; i <= index; i++) {
-            if (i == 2) {
-                j += (i - 2);
-            } else if (i > 2) {
-                j = fibonacciNumbers.get(i - 1) + fibonacciNumbers.get(k - 2);
-            } else {
-                j += i;
-            }
-            fibonacciNumbers.add(j);
-            k++;
-        }
-        return fibonacciNumbers.get(fibonacciNumbers.size() - 1);
+        return (index == 0) ? 0 :
+                (index == 1) ? 1 :
+                        getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 }
 
