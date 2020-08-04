840a8df65082d50df559e886d330932709d9dd12
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-08-04 08:35:04 +0300

    hw

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -12,10 +12,10 @@ public class FibonacciSequence {
     public int getFibonacciNumber(int index) {
         if (index == 0) {
             return 0;
-        } else if (index == 1) {
-            return 1;
-        } else {
-            return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
         }
+        if (index == 1) {
+            return 1;
+        }
+        return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
     }
 }
