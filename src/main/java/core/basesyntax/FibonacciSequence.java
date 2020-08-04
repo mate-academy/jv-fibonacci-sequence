907c93849c481b423af43d2a2529cf990e4811b1
Jah <moskovienkor@gmail.com>
2020-08-04 09:33:58 +0300

    HOMEWORK-6-3: - done;               - fixed(1);               - fixed (2);

--- src/main/java/core/basesyntax/FibonacciSequence.java
+++ src/main/java/core/basesyntax/FibonacciSequence.java
@@ -10,12 +10,6 @@ public class FibonacciSequence {
      * Результат: 21</p>
      */
     public int getFibonacciNumber(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            return getFibonacciNumber(n - 1) + getFibonacciNumber(n - 2);
-        }
+        return n == 0 ? 0 : n == 1 ? 1 : getFibonacciNumber(n - 1) + getFibonacciNumber(n - 2);
     }
 }
